693a0600dc3cfb84f2ef8b3140cd107e97b9c910
Maria Genis <magegr@alumnatflorida.es>
2026-02-11 12:06:50 +0100

    Subiendo carpetas actualizadas con angular

---
+++ Backend/readme.txt
@@ -0,0 +1,1 @@
+aqui lo de symfony
---
+++ Fronted/src/app/views/salud-visual/salud-visual.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salud-visual',
+  imports: [],
+  templateUrl: './salud-visual.html',
+  styleUrl: './salud-visual.css',
+})
+export class SaludVisual {
+
+}
